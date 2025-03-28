3a9d9a67ef56e5ecb3cb2acac75e27f9292246f8
julianMaranta <100044581+julianMaranta@users.noreply.github.com>
2025-03-28 07:56:51 -0300

    Admin

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,6 +7,98 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
+  Usuario: a
+    .model({
+      nombre: a.string().required(),
+      correo: a.string().required(),
+      dni: a.string(), // Nuevo campo para el D.N.I.
+      cuil: a.string(), // Nuevo campo para el CUIL
+      tramite: a.string(), // Nuevo campo para el Número de Trámite
+      contrasena: a.string().required(),
+      rol: a.enum(["Estudiante", "Administrador", "Profesor"]),
+      cursosComprados: a.hasMany("CursoComprado", "usuarioId"),
+      especializacionesCompradas: a.hasMany("EspecializacionComprada", "usuarioId"),
+      tutoriasCompradas: a.hasMany("TutoriaComprada", "usuarioId"),
+      certificadosComprados: a.hasMany("CertificadoComprado", "usuarioId"), // Nuevo campo para certificados comprados
+      comprobantes: a.hasOne("Comprobante", "usuarioId") // Relación hasOne con el campo usuarioId
+     
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+
+    Curso: a
+    .model({
+      nombre: a.string().required(),
+      descripcion: a.string(),
+      linkPago: a.string(),
+      driveLink: a.string(),
+      profesores: a.string().array(), // Se cambia para almacenar múltiples profesores
+      cantidadDeAlumnos: a.integer().required(),
+      clases: a.hasMany("Clase", "cursoId"),
+      cursosComprados: a.hasMany("CursoComprado", "cursoId"),
+      videoUrl: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+
+    Especializacion: a
+    .model({
+      nombre: a.string().required(),
+      descripcion: a.string(),
+      imagen: a.string(),
+      linkPago: a.string(),
+      driveLink: a.string(),
+      profesores: a.string().array(), // Se cambia para almacenar múltiples profesores
+      cantidadDeAlumnos: a.integer().required(),
+      clases: a.hasMany("Clase", "especializacionId"),
+      especializacionesCompradas: a.hasMany("EspecializacionComprada", "especializacionId"),
+      videoUrl: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+
+    CursoComprado: a
+    .model({
+      usuarioId: a.id().required(),
+      cursoId: a.id().required(),
+      usuario: a.belongsTo("Usuario", "usuarioId"),
+      curso: a.belongsTo("Curso", "cursoId"),
+      usuarioNombre: a.string(),  // Nuevo campo
+      usuarioCorreo: a.string(),  // Nuevo campo
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+
+    EspecializacionComprada: a
+    .model({
+      usuarioId: a.id().required(),
+      especializacionId: a.id().required(),
+      usuario: a.belongsTo("Usuario", "usuarioId"),
+      especializacion: a.belongsTo("Especializacion", "especializacionId"),
+      usuarioNombre: a.string(),  // Nuevo campo
+      usuarioCorreo: a.string(),  // Nuevo campo
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+
+    Clase: a
+    .model({
+      nombre: a.string().required(),
+      tematica: a.string().required(),
+      cursoId: a.id(), // Opcional si la clase pertenece a un curso
+      curso: a.belongsTo("Curso", "cursoId"),
+      especializacionId: a.id(), // Opcional si la clase pertenece a una especialización
+      especializacion: a.belongsTo("Especializacion", "especializacionId"),
+      tutoriaId: a.id(), // Opcional si la clase pertenece a una tutoría
+      tutoria: a.belongsTo("Tutoria", "tutoriaId"),
+      fecha: a.date(), // Campo requerido para la fecha de la clase
+      hora: a.time(), // Campo requerido para la hora de la clase
+      youtubeVideoUrl: a.string(),
+      imagenes: a.json(), // Cambiado de string a json para almacenar enlaces y títulos
+      texto: a.string(),
+      zoomLink: a.string(), // Nuevo campo para el link de Zoom
+      driveLinkClase: a.string(),
+      pdfClase: a.string(), 
+      index: a.integer(), // Nuevo campo para almacenar un índice
+      
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+
   Todo: a
     .model({
       content: a.string(),
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,37 @@ import { createRouter, createWebHistory } from 'vue-router';
 import { checkAuth } from '../authenticator/auth';
 
 const routes = [
-  
+  /* RUTAS ADMINISTRADOR - LOGIN */
+  {
+    path: '/login-admin',
+    name: 'login-admin',
+    component: () => import('../views/A_LoginView.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/plataforma-administrador',
+    name: 'plataforma-administrador',
+    component: () => import('../views/A_PlataformaView.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/plataforma-administrador-crear',
+    name: 'plataforma-administrador-crear',
+    component: () => import('../views/A_CreateView.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/plataforma-administrador-editar',
+    name: 'plataforma-administrador-editar',
+    component: () => import('../views/A_EditView.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/plataforma-administrador-eliminar',
+    name: 'plataforma-administrador-eliminar',
+    component: () => import('../views/A_DeleteView.vue'),
+    meta: { requiresAuth: true }
+  },
   {
     path: '/',
     name: 'inicio',
