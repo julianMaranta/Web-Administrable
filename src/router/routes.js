7e3f50111a8712612e2d26146314672ee903d753
julianMaranta <100044581+julianMaranta@users.noreply.github.com>
2025-04-19 20:03:28 -0300

    Ficha Tecnica 1

--- src/router/routes.js
+++ src/router/routes.js
@@ -87,17 +87,10 @@ const routes = [
     name: 'inicio',
     component: () => import('../views/Inicio.vue')
   },
-
-  {
-    path: '/',
-    name: 'property-list',
-    component: PropertyList
-  },
   {
     path: '/propiedad/:type/:id',
-    name: 'property-detail',
-    component: PropertyDetail,
-    props: true
+    name: 'propiedad',
+    component: () => import('../views/PropertyDetail.vue')
   },
  
   {
